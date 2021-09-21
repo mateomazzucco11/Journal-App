94b6b64eabd5908beac524159a4091fc7b23fc92
Mateo <mateomazzucco4@gmail.com>
2021-09-21 09:27:40 -0300

    .env

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -6,15 +6,15 @@ import { getFirestore } from 'firebase/firestore';
 import { GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
-const {REACT_API_KEY, REACT_API_ID, REACT_AUTH_DOMAIN, REACT_PROJECT_ID, REACT_STORAGE_BUCKET, REACT_MESSAGING_SENDER_ID} = process.env
+const {REACT_APP_API_KEY, REACT_APP_API_ID, REACT_APP_AUTH_DOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID} = process.env
 
 const firebaseConfig = {
-  apiKey: REACT_API_KEY,
-  authDomain: REACT_AUTH_DOMAIN,
-  projectId: REACT_PROJECT_ID,
-  storageBucket: REACT_STORAGE_BUCKET,
-  messagingSenderId: REACT_MESSAGING_SENDER_ID,
-  appId: REACT_API_ID
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_API_ID
 };
 
 // Initialize Firebase
